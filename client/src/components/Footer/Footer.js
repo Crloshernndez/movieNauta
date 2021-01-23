182d07173dd7318f26035d2a7889fb38bf248c82
carlos <hernandez1989carlos@gmail.com>
2021-01-23 17:26:21 -0500

    [create] src/assets/images : save logo/ [modify] package.json : install materialUi for styles/ [modify] src/components/Header : styles component/ [modify] src/component/Footer : styles component

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,25 +1,31 @@
 import React from "react";
 import "./Footer.css";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer__information">
-        <p>
-          lorem
+    <>
+      <hr />
+      <div className="footer">
+        <div className="footer__information">
+          <p>
+            lorem
+            <br />
+            lorem
+          </p>
           <br />
-          lorem
-        </p>
-        <br />
-        <hr />
+        </div>
+        <div className="footer__social">
+          <InstagramIcon />
+          <FacebookIcon />
+          <TwitterIcon />
+          <GitHubIcon />
+        </div>
       </div>
-      <div className="footer__social">
-        <img src="" alt="instagram" />
-        <img src="" alt="facebook" />
-        <img src="" alt="twiter" />
-        <img src="" alt="github" />
-      </div>
-    </div>
+    </>
   );
 }
 
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,32 +1,32 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
+import TextField from "@material-ui/core/TextField";
+import Logo from "../../assets/images/logo.svg";
 
 function Header() {
   return (
-    <div className="header">
-      <div className="header__left">
-        <ul>
-          <li>
-            <Link to="/">
-              <img src="" alt="logo" />
-            </Link>
-          </li>
-          <li>
-            <Link to="movies">Movies</Link>
-          </li>
-          <li>
-            <Link to="series">Series</Link>
-          </li>
-          <li>
-            <Link to="aboutUs">AboutUs</Link>
-          </li>
-        </ul>
+    <>
+      <div className="header">
+        <div className="header__left">
+          <Link to="/">
+            <img src={Logo} alt="logo" />
+          </Link>
+          <Link to="movies">Movies</Link>
+          <Link to="series">Series</Link>
+          <Link to="aboutUs">AboutUs</Link>
+        </div>
+        <div className="header__rigth">
+          <TextField
+            className="header__rigth-input"
+            id="outlined-basic"
+            label="Search"
+            variant="outlined"
+          />
+        </div>
       </div>
-      <div className="header__rigth">
-        <input type="text" placeholder="Search" />
-      </div>
-    </div>
+      <hr />
+    </>
   );
 }
 
