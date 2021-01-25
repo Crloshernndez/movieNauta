fdd0a72bcb8f62228b74bab6cf3edc93bd4b2fba
carlos <hernandez1989carlos@gmail.com>
2021-01-25 08:41:49 -0500

    [modified] src/pages/Movies : create css file for component to style it, add Row container/ [modified] src/pages/movies : create css file for component to style it, add Row container

--- client/src/pages/Movies/Movies.js
+++ client/src/pages/Movies/Movies.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "./Movie.css";
+import Row from "../../containers/Row/Row";
+import requests from "../../request/Tmdb";
 
 function Movies() {
   return (
     <div className="movies">
-      <h1>Movie</h1>
+      <h1 className="movies__title">Movie</h1>
+      <Row
+        className="movies__row"
+        fetchUrl={requests.fetchTrending}
+        title="Trending Now"
+      />
     </div>
   );
 }
--- client/src/pages/Series/Series.js
+++ client/src/pages/Series/Series.js
@@ -1,9 +1,18 @@
 import React from "react";
+import "./Series.css";
+
+import Row from "../../containers/Row/Row";
+import requests from "../../request/Tmdb";
 
 function Series() {
   return (
     <div className="series">
-      <h1>Series</h1>
+      <h1 className="series__title">Series</h1>
+      <Row
+        className="movies__row"
+        fetchUrl={requests.fetchTrending}
+        title="Trending Now"
+      />
     </div>
   );
 }
