01c653e51d4052a9e2927d05895dec569c97922a
carlos <hernandez1989carlos@gmail.com>
2021-01-25 19:42:58 -0500

    [modified] package.json : install cors

--- server/api/index.js
+++ server/api/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const config = require("../config");
 const router = require("./network/routes");
+const cors = require("cors");
 
 const app = express();
 app.use(bodyParser.json());
+app.use(cors());
 
 app.listen(config.api.port, () => {
   console.log(`Api listening on ${config.api.host}${config.api.port}`);
